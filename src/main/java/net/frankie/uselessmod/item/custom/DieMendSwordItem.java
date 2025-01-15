3d5c6a5984e6d9d7381428ed0d0682ddcc2bf19b
fj9372 <fj9372@rit.edu>
2025-01-14 20:04:28 -0500

    Die Mend Sword Start

--- src/main/java/net/frankie/uselessmod/item/custom/DieMendSwordItem.java
+++ src/main/java/net/frankie/uselessmod/item/custom/DieMendSwordItem.java
@@ -1,10 +1,33 @@
 package net.frankie.uselessmod.item.custom;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.Level;
 
 public class DieMendSwordItem extends SwordItem {
     public DieMendSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public void onInventoryTick(ItemStack stack, Level level, Player player, int slotIndex, int selectedIndex) {
+        if (!level.isClientSide()) {
+            if(player.getMainHandItem().getItem() == this){
+                return;
+            }
+        }
+        super.onInventoryTick(stack, level, player, slotIndex, selectedIndex);
+    }
+
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
+        player.sendSystemMessage(Component.literal("Durability:" + (this.getMaxDamage(stack) - this.getDamage(stack))));
+        return super.onLeftClickEntity(stack, player, entity);
     }
 }
