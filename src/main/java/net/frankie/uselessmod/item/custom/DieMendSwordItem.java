b0adef81665d25637cd1c450e704c5809d83dba4
fj9372 <fj9372@rit.edu>
2025-01-18 19:03:30 -0500

    Die Mend Sword Charge Attack Started

--- src/main/java/net/frankie/uselessmod/item/custom/DieMendSwordItem.java
+++ src/main/java/net/frankie/uselessmod/item/custom/DieMendSwordItem.java
@@ -1,16 +1,26 @@
 package net.frankie.uselessmod.item.custom;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class DieMendSwordItem extends SwordItem {
+    private int chargeLevel = 0;
+
     public DieMendSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
     }
@@ -18,8 +28,8 @@ public class DieMendSwordItem extends SwordItem {
     @Override
     public void onInventoryTick(ItemStack stack, Level level, Player player, int slotIndex, int selectedIndex) {
         if (!level.isClientSide()) {
-            if(player.getMainHandItem().getItem() == this){
-                return;
+            if(player.getMainHandItem().getItem() == this && this.getDamage(stack) > 0){
+                player.hurt(player.damageSources().playerAttack(player), 2);
             }
         }
         super.onInventoryTick(stack, level, player, slotIndex, selectedIndex);
@@ -27,7 +37,49 @@ public class DieMendSwordItem extends SwordItem {
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        player.sendSystemMessage(Component.literal("Durability:" + (this.getMaxDamage(stack) - this.getDamage(stack))));
+        if (!player.level().isClientSide) {
+            player.sendSystemMessage(Component.literal("Durability:" + (this.getMaxDamage(stack) - this.getDamage(stack))));
+            player.sendSystemMessage(Component.literal("Strength:" + player.getAttackStrengthScale(0.0F)));
+            player.sendSystemMessage(Component.literal("??:" + chargeLevel));
+            chargeLevel = 0;
+        }
         return super.onLeftClickEntity(stack, player, entity);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if(!pLevel.isClientSide){
+            chargeLevel += 1;
+            pLevel.playSeededSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(),
+                    SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.BLOCKS, 1f, 1f, 0);
+        }
+        pPlayer.startUsingItem(pUsedHand);
+        return InteractionResultHolder.consume(pPlayer.getItemInHand(pUsedHand));
+    }
+
+    @Override
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity, int pTimeCharged) {
+        if (pLivingEntity instanceof Player player) {
+            pLevel.playSeededSound(null, player.getX(), player.getY(), player.getZ(),
+                    SoundEvents.ARROW_SHOOT, SoundSource.BLOCKS, 1f, 1f, 0);
+        }
+    }
+
+    @Override
+    public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+        if (pLivingEntity instanceof Player player) {
+            pLevel.playSeededSound(null, player.getX(), player.getY(), player.getZ(),
+                    SoundEvents.ARROW_SHOOT, SoundSource.BLOCKS, 1f, 1f, 0);
+        }
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 72000;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.BOW;
     }
 }
